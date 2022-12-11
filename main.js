573c5089e030267f2134e57104048f98f08c81e8
milonroy26 <101338962+milonroy26@users.noreply.github.com>
2022-12-11 12:43:44 +0600

    only first photo update

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ let first = document.getElementById('first');
 let last = document.getElementById('last');
 let text = document.getElementById('text');
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     let value = window.scrollY;
 
-    first.style.top = value * 0 + 'px';
+    first.style.top = value * 0.0 + 'px';
     last.style.top = value * 0.5 + 'px';
     text.style.top = value * 1 + 'px';
 })
